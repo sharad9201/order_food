8a93b7daf44ba9f4cbab26bf0faa1c9cd81e862b
sharad9201 <sharad.rae19@gmail.com>
2022-04-30 23:27:12 +0545

    final push

---
+++ src/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ItemCard from "./ItemCard";
+import data from "./data";
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-center">All Items</h1>
+      <section className="py-4 container">
+        <div className="row justify-content-center">
+          {data.productData.map((item, index) => {
+            return (
+              <ItemCard
+                img={item.img}
+                title={item.title}
+                desc={item.desc}
+                item={item}
+                price={item.price}
+                key={index}
+              />
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/data.js
@@ -0,0 +1,31 @@
+import img1 from "./img/img1.jpg";
+import img2 from "./img/img2.jpg";
+import img3 from "./img/img3.jpg";
+
+const data = {
+  productData: [
+    {
+      id: 1,
+      img: img1,
+      title: "Chicken Momo",
+      desc: "",
+      price: 140,
+    },
+    {
+      id: 1,
+      img: img2,
+      title: "Chicken Chowmiem",
+      desc: "",
+      price: 160,
+    },
+    {
+      id: 1,
+      img: img3,
+      title: "Chicken Salad",
+      desc: "",
+      price: 140,
+    },
+  ],
+};
+
+export default data;
